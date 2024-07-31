a3169636aff54abac5ffd1f67b936a4023d2fd99
Sandro Muniz <sandrojam@hotmail.com>
2024-07-31 15:13:13 -0300

    Annotation @PathVariable
    Essa annotation tem como função, obter dados de uma url.

--- src/main/java/br/com/projeto/apicliente/controle/Controle.java
+++ src/main/java/br/com/projeto/apicliente/controle/Controle.java
@@ -1,6 +1,7 @@
 package br.com.projeto.apicliente.controle;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,11 @@ public class Controle {
     @GetMapping
     public String mensagem(){
         return "Hello World!";
+    }
+
+    @GetMapping("/boasVindas/{nome}")
+    public String boasVindas(@PathVariable String nome){
+        return "Seja bem vindo(a) " + nome;
     }
 
 }

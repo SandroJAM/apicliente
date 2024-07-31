95706fa9eb98b517981be92df7f6ebc3224961e6
Sandro Muniz <sandrojam@hotmail.com>
2024-07-31 15:38:51 -0300

    Vinculado controle e modelo
    Rota para obter um objeto com as características do modelo, e retornar esse objeto. Duas novas Annotations: @PostMapping: utilizada para criar uma rota com suporte a requisição post e @RequestBody: responsável por obter dados vindos de outras APIs ou do front-end.

--- src/main/java/br/com/projeto/apicliente/controle/Controle.java
+++ src/main/java/br/com/projeto/apicliente/controle/Controle.java
@@ -2,7 +2,11 @@ package br.com.projeto.apicliente.controle;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.apicliente.modelo.Pessoa;
 
 @RestController
 public class Controle {
@@ -15,6 +19,11 @@ public class Controle {
     @GetMapping("/boasVindas/{nome}")
     public String boasVindas(@PathVariable String nome){
         return "Seja bem vindo(a) " + nome;
+    }
+
+    @PostMapping("/pessoa") // O @GetMapping não tem suporte ao @RequestBody
+    public Pessoa pessoa(@RequestBody Pessoa objPessoa){
+        return objPessoa;
     }
 
 }

2ae55d2a96e80d208562da90df5cb04e91eb4dbd
Sandro Muniz <sandrojam@hotmail.com>
2024-08-02 15:28:17 -0300

    ResponseEntity
    Responsável por retornar o status de uma requisição, garantindo que essa requisição especifique exatamente a ação efetuada.

--- src/main/java/br/com/projeto/apicliente/controle/Controle.java
+++ src/main/java/br/com/projeto/apicliente/controle/Controle.java
@@ -3,6 +3,8 @@ package br.com.projeto.apicliente.controle;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -84,6 +86,16 @@ public class Controle {
     @GetMapping("/api/idadeMaiorIgual")
     public List<Pessoa> idadeMaiorIgual(){
         return acao.idadeMaiorIgual(50);
+    }
+
+    /*@GetMapping("/status")
+    public String status(){
+        return "Configurando STATUS";
+    }*/
+
+    @GetMapping("/status")
+    public ResponseEntity<?> status(){
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping

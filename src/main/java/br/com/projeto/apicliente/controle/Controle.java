d3df8ec168dea2f8a640cd77920daf4d038ed67c
Sandro Muniz <sandrojam@hotmail.com>
2024-08-02 13:21:49 -0300

    Contar registros com o comando count()
    O método count() retorna uma informação do tipo long, ele equipara ao comando SQL: SELECT COUNT(*) FROM tabela.

--- src/main/java/br/com/projeto/apicliente/controle/Controle.java
+++ src/main/java/br/com/projeto/apicliente/controle/Controle.java
@@ -16,48 +16,53 @@ import br.com.projeto.apicliente.repositorio.Repositorio;
 
 @RestController
 public class Controle {
-    
+
     @Autowired
     private Repositorio acao;
 
     @PostMapping("/api")
-    public Pessoa cadastrar(@RequestBody Pessoa objPessoa){
+    public Pessoa cadastrar(@RequestBody Pessoa objPessoa) {
         return acao.save(objPessoa);
     }
 
     @GetMapping("/api")
-    public List<Pessoa> selecionar(){
+    public List<Pessoa> selecionar() {
         return acao.findAll();
     }
 
     @GetMapping("/api/{codigo}")
-    public Pessoa selecionarpeloCodigo(@PathVariable int codigo){
+    public Pessoa selecionarpeloCodigo(@PathVariable int codigo) {
         return acao.findByCodigo(codigo);
     }
 
     @PutMapping("/api")
-    public Pessoa editar(@RequestBody Pessoa objPessoa){
+    public Pessoa editar(@RequestBody Pessoa objPessoa) {
         return acao.save(objPessoa);
     }
 
     @DeleteMapping("/api/{codigo}")
-    public void remover(@PathVariable int codigo){
+    public void remover(@PathVariable int codigo) {
         Pessoa objPessoa = selecionarpeloCodigo(codigo);
         acao.delete(objPessoa);
     }
 
+    @GetMapping("/api/contador")
+    public long contador() {
+        return acao.count();
+    }
+
     @GetMapping
-    public String mensagem(){
+    public String mensagem() {
         return "Hello World!";
     }
 
     @GetMapping("/boasVindas/{nome}")
-    public String boasVindas(@PathVariable String nome){
+    public String boasVindas(@PathVariable String nome) {
         return "Seja bem vindo(a) " + nome;
     }
 
     @PostMapping("/pessoa") // O @GetMapping não tem suporte ao @RequestBody
-    public Pessoa pessoa(@RequestBody Pessoa objPessoa){
+    public Pessoa pessoa(@RequestBody Pessoa objPessoa) {
         return objPessoa;
     }
 

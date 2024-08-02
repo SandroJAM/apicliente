faa0f0ae2e10fc39ccaee4f2ac14f4682c491396
Sandro Muniz <sandrojam@hotmail.com>
2024-08-02 15:39:30 -0300

    Annotation @Component
    Utilizada para criar modelos, diferente da annotation @Entity, a @Component não cria uma tabela no banco de dados, podemos manipular um objeto da classe com a annotation @Component via injeção de dependências, utilizando a annotation @Autowired.

--- src/main/java/br/com/projeto/apicliente/servico/Servico.java
+++ src/main/java/br/com/projeto/apicliente/servico/Servico.java
@@ -1,8 +1,14 @@
 package br.com.projeto.apicliente.servico;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.projeto.apicliente.modelo.Mensagem;
 
 @Service
 public class Servico {
+
+    @Autowired
+    private Mensagem mensagem;
     
 }
